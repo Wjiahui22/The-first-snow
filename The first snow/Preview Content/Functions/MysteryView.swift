0b2f9c5bc6c54a622b3857124eeb2125af4b0d87
Wjiahui22 <wjiahui22@gmail.com>
2024-02-18 11:23:29 -0800

    Music

---
+++ The first snow/Preview Content/Functions/MysteryView.swift
@@ -0,0 +1,18 @@
+//
+//  MysteryView.swift
+//  The first snow
+//
+//  Created by Jiahui Chen on 2/18/24.
+//
+
+import SwiftUI
+
+struct MysteryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MysteryView()
+}
---
+++ The first snow/Preview Content/Functions/ThinkView.swift
@@ -0,0 +1,18 @@
+//
+//  ThinkView.swift
+//  The first snow
+//
+//  Created by Jiahui Chen on 2/18/24.
+//
+
+import SwiftUI
+
+struct ThinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThinkView()
+}
